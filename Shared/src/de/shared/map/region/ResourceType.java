eb1eb914aef75c88042a15703b476839f0259790
Matthias Liedtke <mliedtke@gmx.de>
2013-12-23 15:26:10 +0100

    pDepreciationYears added

--- Shared/src/de/shared/map/region/ResourceType.java
+++ Shared/src/de/shared/map/region/ResourceType.java
@@ -3,18 +3,19 @@ package de.shared.map.region;
 import java.io.Serializable;
 
 public enum ResourceType implements Serializable {
-	COAL(10, 0.5, 1, 100000, 10000, 4),
-	GAS(50, 0.05, 1, 200000, 10000, 4),
-	URANIUM(100, 1, 1, 500000, 50000, 8),
-	WIND(5, 1, -1, 50000, 5000, 2),
-	SOLAR(10, 1, -1, 100000, 5000, 4),
-	WATER(1, 0.05, -1, 200, 20, 1),
-	EMPTY(0, 0, 0, 0, 0, 0),
+	COAL(10, 0.5, 1, 100000, 10000, 4, 30),
+	GAS(50, 0.05, 1, 200000, 10000, 4, 20),
+	URANIUM(100, 1, 1, 500000, 50000, 8, 35),
+	WIND(5, 1, -1, 50000, 5000, 2, 16),
+	SOLAR(10, 1, -1, 100000, 5000, 4, 20),
+	WATER(1, 0.05, -1, 200, 20, 1, 40),
+	EMPTY(0, 0, 0, 0, 0, 0, 0),
 	
-	TEST_20(20, 0.2, -1, 100000, 10000, 0),
-	TEST_30(30, 0.2, -1, 100000, 10000, 0),
-	TEST_40(40, 0.2, -1, 90000, 10000, 0);
+	TEST_20(20, 0.2, -1, 100000, 10000, 0, 0),
+	TEST_30(30, 0.2, -1, 100000, 10000, 0, 0),
+	TEST_40(40, 0.2, -1, 90000, 10000, 0, 0);
 	
+	// Values for PowerStations
 	public double pMaxProduction;
 	public double pAdjustability;
 	public int pConsumption;
@@ -22,16 +23,17 @@ public enum ResourceType implements Serializable {
 	public double pMaxRunningCosts;
 	public int pBuildTime;
 	public int pDepreciationYears;
+	// Values for Mines
+	// ...
 	
-	
-	ResourceType(double pMaxProduction, double pAdjustability, int pConsumption, double pPurchaseValue, double pMaxRunningCosts, int pBuildTime) {
+	ResourceType(double pMaxProduction, double pAdjustability, int pConsumption, double pPurchaseValue, double pMaxRunningCosts, int pBuildTime, int pDepreciationYears) {
 		this.pMaxProduction = pMaxProduction;
 		this.pAdjustability = pAdjustability;
 		this.pConsumption = pConsumption;
 		this.pPurchaseValue = pPurchaseValue;
 		this.pMaxRunningCosts = pMaxRunningCosts;
 		this.pBuildTime = pBuildTime;
-		this.pDepreciationYears = Integer.MAX_VALUE; //How is this calculated?
+		this.pDepreciationYears = pDepreciationYears;
 	}
 	
 	
