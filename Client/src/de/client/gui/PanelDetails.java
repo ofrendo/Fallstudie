5cea12242c9a6b849b43dc879189411ae750c33a
Oliver Frendo <ofrendo@gmail.com>
2013-12-23 10:32:12 +0100

    Made an ugly looking GUI

---
+++ Client/src/de/client/gui/PanelDetails.java
@@ -0,0 +1,17 @@
+package de.client.gui;
+
+import java.awt.Color;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class PanelDetails extends JPanel {
+
+	private static final long serialVersionUID = -3177233247088338481L;
+
+	public PanelDetails() {
+		setBackground(Color.RED);
+		add(new JLabel("Details"));
+	}
+	
+}
