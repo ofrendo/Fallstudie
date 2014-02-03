09867e299431e03826c311a1db9664132213862f
Joern <Joern@141.72.114.145>
2014-02-03 14:32:51 +0100

    pushed number changes

--- Shared/src/de/shared/game/Constants.java
+++ Shared/src/de/shared/game/Constants.java
@@ -4,20 +4,20 @@ public class Constants {
 	public static final double ENERGY_FACTOR = 0.4;
 	
 	public static double START_POPULARTIY = 0.5;
-	public static double START_AWARENESS = 0.1;
-	public static double START_MONEY = 5000000;	// Leute, macht euch mal Gedanken, mit wie viel "money in the tasche" man starten soll
+	public static double START_AWARENESS = 0.5;
+	public static double START_MONEY = 650000000;
 	
 	public static int MAX_POWERSTATION_DISTANCE = 3;
 	
 	//Costs and prices
-	public static double NET_USAGE_COSTS = 0.06;
+	public static double NET_USAGE_COSTS = 6;
 	public static double NORMAL_ENERGY_PRICE = 200; //Normal energy price: this is the price when demand equals offers 
 	
 	//Resource Costs
 	public static double COST_COAL = 20;
 	public static double COST_GAS = 25;
 	public static double COST_URANIUM = 50;
-	public static double MINIMUM_REGION_BID = 100000;
+	public static double MINIMUM_REGION_BID = 10000000;
 	
 	// Storing costs for the warehouse per ton
 	public static double STORING_COSTS = 100;
